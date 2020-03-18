9a7b9156c9a8347121df3ab4dd1014c0e0e3acc7
David Haukeness <david@hauken.us>
2020-03-18 18:55:45 +0000

    removed extra info, link and pin should be sufficient for now

--- commands.go
+++ commands.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
-	"text/tabwriter"
 
 	"samhofi.us/x/keybase/types/chat1"
 )
@@ -17,17 +15,6 @@ func (b *bot) setupMeeting(convid chat1.ConvIDStr, msgid chat1.MessageID, words 
 		b.k.SendMessageByConvID(convid, "I'm sorry, i'm not sure what happened... I was unable to set up a new meeting.\nI've written the appropriate logs and notified my humans.")
 		return
 	}
-	var buf bytes.Buffer
-	w := tabwriter.NewWriter(&buf, 0, 4, 3, ' ', 0)
-	fmt.Fprintln(w, "Here's your meeting:")
-	fmt.Fprintf(w, "URL:\t%s\n", meeting.getURL())
-	fmt.Fprintf(w, "PIN:\t%s\n", meeting.getPIN())
-	fmt.Fprintln(w, "Dial In:\t")
-	fmt.Fprintln(w, "```")
-	for _, phone := range meeting.Phone {
-		fmt.Fprintf(w, "    %s\t%s\t\n", phone.Country, phone.Number)
-	}
-	fmt.Fprintln(w, "```")
-	w.Flush()
-	b.k.SendMessageByConvID(convid, buf.String())
+	message := fmt.Sprintf("Here's your meeting:\n>URL: %s\n>PIN: %s", meeting.getURL(), meeting.getPIN())
+	b.k.SendMessageByConvID(convid, message)
 }
