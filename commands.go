66f9dc6d6810d1d19d3c00207207d215cebfb834
David Haukeness <david@hauken.us>
2020-03-24 22:40:33 +0000

    string join does need spaces for human readability

--- commands.go
+++ commands.go
@@ -24,7 +24,7 @@ func (b *bot) setupMeeting(convid chat1.ConvIDStr, sender string, words []string
 func (b *bot) sendFeedback(convid chat1.ConvIDStr, mesgID chat1.MessageID, sender string, words []string) {
 	b.debug("feedback recieved in %s", convid)
 	if b.config.FeedbackConvIDStr != "" {
-		feedback := strings.Join(words[2:], "")
+		feedback := strings.Join(words[2:], " ")
 		fcID := chat1.ConvIDStr(b.config.FeedbackConvIDStr)
 		if _, err := b.k.SendMessageByConvID(fcID, "Feedback from @%s:\n```%s```", sender, feedback); err != nil {
 			b.k.ReplyByConvID(convid, mesgID, "I'm sorry, I was unable to send your feedback because my benevolent overlords have not set a destination for feedback. :sad:")
