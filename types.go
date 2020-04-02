d6499bb2c2be64ec047806d8ad85ef9428cda6dd
David Haukeness <david@hauken.us>
2020-04-02 22:43:33 +0000

    removed notofications for now

--- types.go
+++ types.go
@@ -10,7 +10,7 @@ type botReply struct {
 
 // ConvOptions stores team specific options like custom servers
 type ConvOptions struct {
-	ConvID               string `json:"converation_id,omitempty"`
-	NotificationsEnabled bool   `json:"notifications_enabled,omitempty"`
-	CustomURL            string `json:"custom_url,omitempty"`
+	ConvID string `json:"converation_id,omitempty"`
+	//NotificationsEnabled bool   `json:"notifications_enabled,omitempty"`
+	CustomURL string `json:"custom_url,omitempty"`
 }
